39bf4c4750bf9b726c19adec5121c9ea7f2075b8
zjzhu <zjzhu@sedwt.com.cn>
2017-07-28 17:17:00 +0800

    新闻内容详情嵌套滚动实现（未完成）

--- app/src/main/java/com/snick/zzj/t_reader/Behavior/NewsContentBehavior.java
+++ app/src/main/java/com/snick/zzj/t_reader/Behavior/NewsContentBehavior.java
@@ -3,13 +3,17 @@ package com.snick.zzj.t_reader.Behavior;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+
+import com.snick.zzj.t_reader.R;
+import com.snick.zzj.t_reader.views.NestedScrollWebView;
 
 /**
  * Created by zzj on 17-6-22.
  */
 
-public class NewsContentBehavior extends CoordinatorLayout.Behavior {
+public class NewsContentBehavior extends CoordinatorLayout.Behavior<NestedScrollWebView> {
     private Context context;
 
     //一定要实现这个构造函数，否则会报Could not inflate Behavior subclass xxx 异常，可查看源码
@@ -19,12 +23,25 @@ public class NewsContentBehavior extends CoordinatorLayout.Behavior {
     }
 
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
-        return super.layoutDependsOn(parent, child, dependency);
+    public boolean layoutDependsOn(CoordinatorLayout parent, NestedScrollWebView child, View dependency) {
+        return dependency != null && dependency.getId() == R.id.anchor;
     }
 
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
-        return super.onDependentViewChanged(parent, child, dependency);
+    public boolean onDependentViewChanged(CoordinatorLayout parent, NestedScrollWebView child, View dependency) {
+        if(dependency.getTranslationY() == 0) {
+            child.setTranslationY(getInitOffset());
+        } else {
+            child.setTranslationY(child.getTranslationY() + dependency.getTranslationY());
+        }
+        return false;
+    }
+
+    private int getInitOffset() {
+        return context.getResources().getDimensionPixelOffset(R.dimen.news_content_init_offset);
+    }
+
+    private int getHeaderOffset() {
+        return context.getResources().getDimensionPixelOffset(R.dimen.news_header_offset_range);
     }
 }
