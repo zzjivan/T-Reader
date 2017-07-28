0b7ca2c92afb439eee0c85523f9cef519097a978
zjzhu <zjzhu@sedwt.com.cn>
2017-07-28 17:18:39 +0800

    新闻内容详情嵌套滚动实现（未完成）

--- app/src/main/java/com/snick/zzj/t_reader/Behavior/NewsHeaderBehavior.java
+++ app/src/main/java/com/snick/zzj/t_reader/Behavior/NewsHeaderBehavior.java
@@ -3,13 +3,15 @@ package com.snick.zzj.t_reader.Behavior;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
 
 /**
  * Created by zzj on 17-6-22.
  */
 
-public class NewsHeaderBehavior extends CoordinatorLayout.Behavior {
+public class NewsHeaderBehavior extends CoordinatorLayout.Behavior<FrameLayout> {
     private Context context;
 
     //一定要实现这个构造函数，否则会报Could not inflate Behavior subclass xxx 异常，可查看源码
@@ -19,7 +21,21 @@ public class NewsHeaderBehavior extends CoordinatorLayout.Behavior {
     }
 
     @Override
-    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed) {
-        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed);
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FrameLayout child, View directTargetChild, View target, int nestedScrollAxes) {
+        return true;
+    }
+
+    //手指上滑，dy>0
+    //手指下滑, dy<0
+    @Override
+    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, FrameLayout child, View target, int dx, int dy, int[] consumed) {
+        //Log.d("zjzhu","header:"+dy);
+        child.setTranslationY(child.getTranslationY() - dy/4);
+        consumed[1] = dy/4;//consumed数组中必须和dy同符号，否则dispatchNestedPreScroll会返回false
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FrameLayout child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
     }
 }
--- app/src/main/java/com/snick/zzj/t_reader/Behavior/ToolbarBehavior.java
+++ app/src/main/java/com/snick/zzj/t_reader/Behavior/ToolbarBehavior.java
@@ -1,15 +1,19 @@
 package com.snick.zzj.t_reader.Behavior;
 
 import android.content.Context;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+
+import com.snick.zzj.t_reader.R;
 
 /**
  * Created by zzj on 17-6-22.
  */
 
-public class ToolbarBehavior extends CoordinatorLayout.Behavior {
+public class ToolbarBehavior extends CoordinatorLayout.Behavior<AppBarLayout> {
     private Context context;
 
     //一定要实现这个构造函数，否则会报Could not inflate Behavior subclass xxx 异常，可查看源码
@@ -19,12 +23,24 @@ public class ToolbarBehavior extends CoordinatorLayout.Behavior {
     }
 
     @Override
-    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
-        return super.layoutDependsOn(parent, child, dependency);
+    public boolean layoutDependsOn(CoordinatorLayout parent, AppBarLayout child, View dependency) {
+        return dependency != null && dependency.getId() == R.id.tbsContent;
     }
 
     @Override
-    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
-        return super.onDependentViewChanged(parent, child, dependency);
+    public boolean onDependentViewChanged(CoordinatorLayout parent, AppBarLayout child, View dependency) {
+        Log.d("zjzhu","toolbar:"+dependency.getTranslationY());
+        if(dependency.getTranslationY() <= 0 && dependency.getTranslationY() >= -getAlphaChangeOffsetRange()) {
+            child.setAlpha((float) (Math.abs(dependency.getTranslationY()) * 255 / getAlphaChangeOffsetRange()));
+        }
+        return false;
+    }
+
+    /**
+     * Header移动real_height距离过程中，toolbar是改变透明度
+     * @return
+     */
+    private int getAlphaChangeOffsetRange() {
+        return context.getResources().getDimensionPixelOffset(R.dimen.news_header_real_height);
     }
 }
